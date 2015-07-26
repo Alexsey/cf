a6d8b830fc7527949e0891506e4a65ffc963f071
Aleksey Bobyr <agat00@gmail.com>
2015-07-26 12:40:59 +0300

    add tests and params parsing

--- bin/cf_.js
+++ bin/cf_.js
@@ -7,14 +7,47 @@ var args = process.argv.slice(2)
 var pwd = process.env.PWD
 
 var codeFileName = args[0]
+var codeStr = fs.readFileSync(codeFileName, 'utf8').replace(/\r/g, '')
+
+// todo read test files async
 var testFileName = args[1]
-testFileName = testFileName
-  || codeFileName.slice(-3) == '.js' && readFile(codeFileName.slice(0, -3))
-  || readFile(codeFileName.slice(0, -2) + 'test')
+var testsStr = readFile(testFileName)
+  || readFile(codeFileName.slice(0, -3)) // 1A.js -> 1A
+  || readFile(codeFileName.slice(0, -2) + 'test') // 1A.js -> 1A.test
   || readFile('tests')
   || readFile('test')
 
+// todo read tests from code file
+
 function readFile (fileName) {
-  try {return fs.readFileSync(fileName, 'utf8') || ' '} catch (e) {}
-  //try {return fs.readFileSync(pwd + '/' + fileName, 'utf8') || ' '} catch (e) {}
+  try {
+    return fs.readFileSync(fileName, 'utf8').replace(/\r/g, '') || ' '
+  } catch (e) {}
 }
+
+if (!testsStr) throw 'no tests found'
+
+var inputs = []
+var outputs = []
+testsStr = testsStr.replace(/\n{3,}/g, '\n\n').trim()
+testsStr.split('\n\n').reverse().forEach(function (paragraph, i) {
+  (i % 2 ? inputs : outputs).push(paragraph)
+})
+
+var tests = []
+var input
+while (input = inputs.shift()) tests.push({input: input, output: outputs.shift()})
+tests.reverse()
+var parameters = outputs.shift()
+
+var testsRunOnly = []
+var testsCommon = []
+tests.forEach(function (test) {
+  if (test.input[0] == '+') {
+    test.input = test.input.slice(2)
+    testsRunOnly.push(test)
+  } else if (test.input[0] != '-') {
+    testsCommon.push(test)
+  }
+})
+tests = testsRunOnly.length ? testsRunOnly : testsCommon
