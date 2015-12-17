4b3611e50eb61217f4a26ecd8c31e5dc63bcc855
Aleksey Bobyr <agat00@gmail.com>
2015-12-18 01:57:41 +0200

    add BabelJS. Also. Minor improvments

--- cf.js
+++ cf.js
@@ -1,38 +1,29 @@
-#!/usr/bin/env node
 'use strict'
 
-let fs = require('fs')
-let path = require('path')
+const fs = require('fs')
+const path = require('path')
 
-process.stdout.isTTY = true
+process.stdout.isTTY = true // some terminals need this to enable color output
 require('colors')
-let _ = require('lodash') || false // hacking WebStorm syntax highlight bug
+const _ = require('lodash') || false // hacking WebStorm syntax highlight bug
 
 
-let code = readCodeFile()
-let main = new Function ('readline', 'write', 'print', code)
-
-//let {testsToRun, testsQuantity, params} = parseTestsFile()
-let testsParseResult = parseTestsFile()
-let testsToRun = testsParseResult.testsToRun
-let testsQuantity = testsParseResult.testsQuantity
-let params = testsParseResult.params
-
-let failedTests = runTests(main, testsToRun, params)
-
+const code = readCodeFile()
+const main = new Function ('readline', 'write', 'print', code)
+const {testsToRun, testsQuantity, params} = parseTestsFile()
+const failedTests = runTests(main, testsToRun, params)
 printWarnings(code, testsToRun, failedTests, testsQuantity, params)
-
 printFailedResults(failedTests)
 
 
 function readCodeFile () {
-	let rawCodeFilePath = process.argv[2]
+	const rawCodeFilePath = process.argv[2]
 	if (!rawCodeFilePath) terminate('provide code file as first argument')
-	let codeFilePath = formatCodeFilePath(rawCodeFilePath)
-	let code = readFile(codeFilePath)
+	const codeFilePath = formatCodeFilePath(rawCodeFilePath)
+	const code = readFile(codeFilePath)
 
 	if (!code) {
-		let codeFileFullPath = path.join(process.cwd(), codeFilePath)
+		const codeFileFullPath = path.join(process.cwd(), codeFilePath)
 		terminate(`ENOENT: no such file or directory '${codeFileFullPath}'`)
 	}
 
@@ -42,10 +33,10 @@ function readCodeFile () {
 }
 
 function parseTestsFile () {
-	let testFilePath = process.argv[3]
-	let codeFilePath = formatCodeFilePath(process.argv[2])
-	// todo read test files async
-	let testsStr = readFile(testFilePath)
+	const testFilePath = process.argv[3]
+	const codeFilePath = formatCodeFilePath(process.argv[2])
+	// may be I will read test files async with async/await. May be
+	const testsStr = readFile(testFilePath)
 		|| readFile(codeFilePath.slice(0, -3)) // 1A.js -> 1A
 		|| readFile(codeFilePath.slice(0, -2) + 'test') // 1A.js -> 1A.test
 		|| readFile('tests')
@@ -55,15 +46,15 @@ function parseTestsFile () {
 
 	if (!testsStr) terminate('file with tests not found')
 
-	let paragraphs = _(testsStr
+	const paragraphs = _(testsStr
 		.split('\n')).invoke('trim').join('\n').trim()
 		.replace(/\n{3,}/g, '\n\n').split('\n\n')
 
-	let testParagraphs = paragraphs.slice(paragraphs.length & 1)
-	let paramsLine = paragraphs.slice(0, paragraphs.length & 1)[0]
+	const testParagraphs = paragraphs.slice(paragraphs.length % 2)
+	const paramsLine = paragraphs.slice(0, paragraphs.length % 2)[0]
 
-	let tests = parseTests(testParagraphs)
-	let params = parseParams(paramsLine)
+	const tests = parseTests(testParagraphs)
+	const params = parseParams(paramsLine)
 
 	return {
 		testsToRun: tests.testsToRun,
@@ -72,35 +63,29 @@ function parseTestsFile () {
 	}
 
 	function parseTests (paragraphs) {
-		let tests = _(paragraphs).chunk(2)
-			.map(v => ({input: v[0], expectation: v[1]}))
-		//	.map(([input, expectation]) => ({input, expectation}))
-			.value()
+		const tests = _.chunk(paragraphs, 2)
+				.map(([input, expectation]) => ({input, expectation}))
 
-		//let {testsRunOnly, testsCommon} = _.groupBy(tests, v => {
-		let sortedTests = _.groupBy(tests, v => {
+		const {testsRunOnly, testsCommon} = _.groupBy(tests, v => {
 			switch (v.input[0]) {
-				case '+':	return 'runOnly'
-				case '-': return 'skip'
-				default : return 'run'
+				case '+':	return 'testsRunOnly'
+				case '-': return 'testsSkip'
+				default : return 'testsCommon'
 			}
 		})
-		var testsRunOnly = _.forEach(sortedTests.runOnly,
-				v => v.input = v.input.slice(1).trimLeft()
-		)
-		var commonTests = sortedTests.run
-		let testsToRun = testsRunOnly || commonTests
+		_.forEach(testsRunOnly, v => v.input = v.input.slice(1).trimLeft())
+		//_.forEach(testsSkip, v => v.input = v.input.slice(1).trimLeft())
+		const testsToRun = testsRunOnly || testsCommon
 		if (!testsToRun) terminate('no tests to run')
 
 		return {
-			testsToRun: testsToRun,
+			testsToRun,
 			testsQuantity: tests.length
 		}
 	}
 
-	//function parseParams (paramsLine = '') {
-	function parseParams (paramsLine) {
-		return _(_(paramsLine || '')         // ' n   e =2 '
+	function parseParams (paramsLine = '') {
+		return _(_(paramsLine)               // ' n   e =2 '
 			.split('=')                        // [' n   e ', '2 ']
 			.invoke('trim')                    // ['n   e', '2']
 			.join('=')                         // ['n   e=2']
@@ -115,20 +100,24 @@ function parseTestsFile () {
 
 function runTests (main, tests, params) {
 	// think over better form, may be without explicit failedTests
-	let failedTests = []
+	const failedTests = []
 	tests.forEach(test => {
-		// make them immutable for consistency?
 		let actual = ''
-		let input = test.input.split('\n').reverse()
-		let readline = () => input.pop()
-		let write = str => {actual += str}
-		let print = str => {actual += str + '\n'}
+		const input = test.input.split('\n').reverse()
+		const readline = () => input.pop()
+		const write = str => actual += str
+		const print = str => actual += str + '\n'
 
 		main(readline, write, print)
 
 		// todo may be support some extra character for output to just print the result?
-		// todo last character must be '\n' in both cases
-		if (params.e
+		if (!actual.endsWith('\n')) {
+			failedTests.push({
+				actual: actual.trim(),
+				expectation: 'test output must ends with \\n',
+				input: test.input
+			})
+		} else if (params.e
 				? Math.abs(test.expectation - actual) >= Math.pow(10, -params.e)
 				: actual != test.expectation + '\n'
 		)
@@ -150,15 +139,20 @@ function printWarnings (code, ranTests, failedTests, testsQuantity, params) {
 }
 
 function printFailedResults (failedTests) {
-	failedTests.forEach(function (fail) {
-		let expectations = fail.expectation.split('\n').reverse()
-		let actuals      = fail.actual     .split('\n').reverse()
-		let inputs       = fail.input      .split('\n').reverse()
+	failedTests.forEach(function (failedTest) {
+		const expectations = failedTest.expectation.split('\n').reverse()
+		const actuals      = failedTest.actual     .split('\n').reverse()
+		const inputs       = failedTest.input      .split('\n').reverse()
 
-		if (params.n) {expectations.push(''); actuals.push('')}
+		// todo pad expectations and actuals to inputs with ''
+		console.log('!!!!!', inputs.length, expectations.length)
+		if (inputs.length - expectations.length == 1) {
+			expectations.push('')
+			actuals.push('')
+		}
 
-		let expectationWidth = _(expectations).pluck('length').max() + 3
-		let inputWidth       = _(inputs)      .pluck('length').max() + 3
+		const expectationWidth = _(expectations).map('length').max() + 3
+		const inputWidth       = _(inputs)      .map('length').max() + 3
 
 		while (inputs.length || expectations.length || actuals.length)
 			console.log(
