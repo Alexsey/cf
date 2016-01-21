5830c68441d264857c7f6906a1af3985c7ef37c2
Aleksey Bobyr <alexsey.bobyr@lifestreet.com>
2016-01-21 16:41:32 +0200

    fix wrong split by paragraphs of test file

--- cf.js
+++ cf.js
@@ -45,7 +45,7 @@ function parseTestsFile () {
 
   if (!testsStr) terminate('file with tests not found')
 
-  const paragraphs = testsStr.trim().split(/\s*[\n\s*]{2,}/g)
+  const paragraphs = testsStr.trim().split(/\s*(?:\n\s*){2,}/g)
   const testParagraphs = paragraphs.slice(paragraphs.length % 2)
   const paramsLine = paragraphs.slice(0, paragraphs.length % 2)[0]
 
